7c8e87e50e87f5a7c62d97cb9fb66a8c2dcae09b
razor7877 <razor7877@gmail.com>
2023-03-11 15:27:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# ThreeJS Quick Start
+ A very basic npm project to quickly prototype with Three.js
+
+ This includes a basic JavaScript file that sets up a basic scene:
+ - Camera with orbit controls
+ - A test mesh to make sure everything works correctly
+ - Window resize callback function
+ - Render loop and delta time variables
+
+ To use it, clone/download the repository, run "npm install" to download the dependencies, then use "npm run start" to start a local server (using Parcel).
---
+++ script.js
@@ -0,0 +1,67 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+// DOM Elements
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 12000);
+const controls = new OrbitControls(camera, canvas);
+camera.position.z = 5;
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha: true,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setClearAlpha(0);
+
+// Callbacks
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// Test mesh
+const torusKnot = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+
+);
+scene.add(torusKnot);
+
+// Render loop
+const clock = new THREE.Clock();
+let previousTime = 0;
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+    const deltaTime = elapsedTime - previousTime;
+    previousTime = elapsedTime;
+
+    controls.update();
+
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(tick);
+};
+
+tick();
